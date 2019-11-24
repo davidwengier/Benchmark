c281cbaea2e98fd6a3165984c9443fdc1ee0b7ab
David Wengier <git@wengier.com>
2019-11-25 09:23:50 +1100

    Interpolated strings benchmark from twitter

---
+++ BenchmarkCore/InterpolatedStrings.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkCore
+{
+    // From: https://twitter.com/mattleibow/status/1198721425785466883
+    [MemoryDiagnoser]
+    public class InterpolatedStrings
+    {
+        [Benchmark]
+        public string IntToString()
+        {
+            return $"b{1.ToString()}b";
+        }
+
+        [Benchmark]
+        public string Int()
+        {
+            return $"a{1}a";
+        }
+    }
+}
