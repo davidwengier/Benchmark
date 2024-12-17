6271d732da86e274d10ce3e583eb932ee8857c86
David Wengier <git@wengier.com>
2024-12-17 14:08:55 +1100

    Bump (and optionally, grind)

---
+++ BenchmarkCore/Collections.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkCore
+{
+    [MemoryDiagnoser]
+    public class Collections
+    {
+        private ImmutableArray<int> _immutableArray;
+        private IEnumerable<int> _enumerable;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _immutableArray = [.. Enumerable.Range(0, 1000)];
+            _enumerable = _immutableArray;
+        }
+
+        [Benchmark]
+        public long ImmutableArray()
+        {
+            var c = 0;
+            foreach (var i in _immutableArray)
+            {
+                c++;
+            }
+
+            return c;
+        }
+
+        [Benchmark]
+        public long IEnumerable()
+        {
+            var c = 0;
+            foreach (var i in _enumerable)
+            {
+                c++;
+            }
+
+            return c;
+        }
+    }
+}
